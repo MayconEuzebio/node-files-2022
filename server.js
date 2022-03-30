949f068159419a1faca373bf030c0d0262fbcfb5
MayconEuzebio <euzebio.maycon@gmail.com>
2022-03-30 00:14:34 -0300

    Server js optimization code

--- server.js
+++ server.js
@@ -3,29 +3,19 @@ const https = require('https');
 https.get('https://coderbyte.com/api/challenges/json/age-counting', (resp) => {
   
   let data = '',
-      arr,
-      count;
+      arr = [],
+      count = 0;
 
-  // parse json data here...
   resp.on('data', (foo) => {
     data += foo;
-    count = 0;
-    //data = data.replace(/{"data":"/g, '').replace(/"}/g, '');
-    //data = data.replace(/{"data":"/g, '').replace(/"}/g, '').replace(/=/g, ':').replace(/ /g, '');
-    //data = '{' + data + '}';    
-    arr = data.split(/,|=/);
-    arr.map((value) => {
-        if(!isNaN(value) && value >= 50) count++;
-    })
-    console.log('on --> ' + count);
   });
-  
 
   resp.on('end', () => {
-    //console.log(JSON.parse(data).explanation);
-    //console.log('end');
-    console.log('end --> ' + count);
-    return count;
+    arr = JSON.parse(data).data.split(/,|=/);
+    arr.map((value) => {
+      if(!isNaN(value) && value >= 50) { count++; }
+    });
+    console.log(count);
   });
 
 }).on('error', (err) => {
